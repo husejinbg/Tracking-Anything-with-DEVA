2f5e5178287123b842e5aaca0c318bddde721be6
husejinbg <husejinbg@gmail.com>
2024-11-24 00:58:22 +0300

    process_single_frame function returns the masks generated by the model.

--- xtrack.py
+++ xtrack.py
@@ -13,6 +13,8 @@ from deva.ext.grounding_dino import get_grounding_dino_model
 from deva.ext.with_text_processor import process_frame_with_text as process_frame_text
 import cv2
 
+import json
+
 
 class XTrack:
     def __init__(self, args, cfg: dict, output_root: str, video_name: str, mask_alpha: float = 0.5,
@@ -23,6 +25,10 @@ class XTrack:
         self.fps = fps
         self.ti = 0
         self.output_video_path = output_video_path
+        self.output_root = output_root
+        self.video_name = video_name
+        self.visualize_postfix = 'Visualizations'
+        self.output_postfix = 'Annotations'
         
         torch.autograd.set_grad_enabled(False)
 
@@ -69,8 +75,31 @@ class XTrack:
             
             self.ti += 1
 
+        
+
     def finish(self):
         with torch.cuda.amp.autocast(enabled=self.cfg['amp']):
             flush_buffer(self.deva, self.result_saver)
         self.writer.release()
         self.deva.clear_buffer()
+
+    def _retrieve_results(self, frame_name: str):
+        output_path = path.join(self.output_root, self.output_postfix, self.video_name)
+        visualizations_path = path.join(self.output_root, self.visualize_postfix, self.video_name)
+        frame_name_no_ext = frame_name.split('.')[0]
+        with open(path.join(output_path, f"{frame_name_no_ext}_info.json"), 'r') as f:
+            info = json.load(f)
+
+        mask_data = []
+
+        for key, value in info.items():
+            mask_name = key
+            label = " ".join(value.split()[0:-1])
+            confidence = float(value.split()[-1])
+            mask_img = cv2.imread(path.join(output_path, mask_name))
+            mask_data.append((mask_img, label, confidence))
+
+        return mask_data
+
+
+
